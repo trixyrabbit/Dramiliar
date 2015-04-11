a180b89b4e65a896bfd486c3650e8f74ee71e1b0
Ryan Murphy <murphy2902@gmail.com>
2015-04-11 05:36:32 -0400

    Added OpenCV section of controller.js

--- controller.js
+++ controller.js
@@ -8,11 +8,22 @@ var lastPng;
 pngStream
 .on('error', console.log)
 .on('data', function(pngBuffer) {
-  lastPng = pngBuffer;
+  //lastPng = pngBuffer;
 
   //Instead of fs.write, cv.readImage TODO
-  fs.writeFile('./img/' + Date.now() + '.png', pngBuffer, function (err) {
-    if (err) throw err;
-    console.log('It\'s saved!');
-  });
+  //fs.writeFile('./img/' + Date.now() + '.png', pngBuffer, function (err) {
+    //if (err) throw err;
+    //console.log('It\'s saved!');
+  //});
+
+  cv.readImage("./pngBuffer", function(err, im){
+		im.detectObject(cv.FACE_CASCADE, {}, function(err, faces){
+			for (var i=0;i<faces.length; i++){
+				var x = faces[i]
+				im.ellipse(x.x + x.width/2, x.y + x.height/2, x.width/2, x.height/2);
+			}
+			
+			im.save('./out.png');
+		});
+  })
 });
